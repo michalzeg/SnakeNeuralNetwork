c8a8dee2ae909a238d4f8e1db60f67a1444067e6
mch <michal.zeg@gmail.com>
2024-05-20 17:11:01 +0200

    implemented duration time of the calculations

--- web/src/app/training/training.component.ts
+++ web/src/app/training/training.component.ts
@@ -9,7 +9,7 @@ import { v4 as uuidv4 } from 'uuid';
   styleUrls: ['./training.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TrainingComponent implements OnInit{
+export class TrainingComponent implements OnInit {
 
   modelName = uuidv4();
 
@@ -17,39 +17,60 @@ export class TrainingComponent implements OnInit{
   currentLength = 0;
   episode = 0;
   actionCount = 0;
+  startTime?: Date;
 
-  episodeSnakeLengthMap: Map<number,number> = new Map<number,number>();
+  episodeSnakeLengthMap: Map<number, number> = new Map<number, number>();
   snakeLengthStats: { length: number, count: number }[] = [];
 
   constructor(
     private domainService: DomainService,
     private readonly detector: ChangeDetectorRef
-    ) {
+  ) {
 
   }
   ngOnInit(): void {
-    this.domainService.progress$.subscribe(e=>{
+    this.domainService.progress$.subscribe(e => {
       this.currentLength = e.snakeLength;
       this.maxSnakeLength = this.maxSnakeLength < e.snakeLength ? e.snakeLength : this.maxSnakeLength;
       this.episode = e.episode;
       this.actionCount = e.actionCount;
-      this.updateStats(e.episode,e.snakeLength);
+      this.updateStats(e.episode, e.snakeLength);
       this.detector.detectChanges();
     });
   }
 
-  public async train(): Promise<void>{
-    await this.domainService.train(this.modelName);
+  public get duration(): string {
+
+    if (!this.startTime) {
+      return ""
+    }
+
+    const end = new Date();
+    const durationMs = end.getTime() - this.startTime.getTime();
+
+    const seconds = Math.floor((durationMs / 1000) % 60);
+    const minutes = Math.floor((durationMs / (1000 * 60)) % 60);
+    const hours = Math.floor((durationMs / (1000 * 60 * 60)) % 24);
+
+    const pad = (num: number): string => num.toString().padStart(2, '0');
+    const durationString = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+
+    return durationString;
   }
 
-  private updateStats(episode: number, snakeLength: number){
-      this.episodeSnakeLengthMap.set(episode, snakeLength);
+  public async train(): Promise<void> {
+    await this.domainService.train(this.modelName);
+    this.startTime = new Date();
+  }
 
-      const groupedValues = [...this.episodeSnakeLengthMap.values()].reduce((countMap, num) => {
-        countMap.set(num, (countMap.get(num) || 0) + 1);
-        return countMap;
-      }, new Map<number, number>())
+  private updateStats(episode: number, snakeLength: number) {
+    this.episodeSnakeLengthMap.set(episode, snakeLength);
 
-      this.snakeLengthStats = [...groupedValues.entries()].map(([length, count]) => ({length, count})).sort((a,b)=>a.length - b.length);
+    const groupedValues = [...this.episodeSnakeLengthMap.values()].reduce((countMap, num) => {
+      countMap.set(num, (countMap.get(num) || 0) + 1);
+      return countMap;
+    }, new Map<number, number>())
+
+    this.snakeLengthStats = [...groupedValues.entries()].map(([length, count]) => ({ length, count })).sort((a, b) => a.length - b.length);
   }
 }
